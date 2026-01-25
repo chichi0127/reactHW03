5ffe88b27931d12203b38a43fb9986ac26eccd1f
YUCCC <qq7002tw@gmail.com>
2026-01-25 23:32:00 +0800

    完成登入登出API

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/react_HW02/' : '/',
   plugins: [react()],
 })
