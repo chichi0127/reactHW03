a634ea5b8aaca8c27a482de5e58e02e967fba3e1
qq7002tw@gmail.com <qq7002tw@gmail.com>
2026-02-16 00:39:17 +0800

    改一下路徑

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/reactHW02/' : '/',
+  base: '/reactHW02/',
+  // base: process.env.NODE_ENV === 'production' ? '/reactHW02/' : '/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
