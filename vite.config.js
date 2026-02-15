35b2eeffe11fc2ac06f43a03277639ff711875e9
qq7002tw@gmail.com <qq7002tw@gmail.com>
2026-02-16 00:30:53 +0800

    page有問題?

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/react_HW02/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/reactHW02/' : '/',
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import'],
+      },
+    },
+  },
 })
