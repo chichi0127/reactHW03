f87d3d1083cb48e600abfe43ef4e2ff48c988025
qq7002tw@gmail.com <qq7002tw@gmail.com>
2026-02-17 09:59:24 +0800

    複製week2作業

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
 
-  base: process.env.NODE_ENV === 'production' ? '/reactHW02/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/reactHW03/' : '/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
