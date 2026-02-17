9f3bb578d8362ef711ce9b8f054d73a98720faf2
qq7002tw@gmail.com <qq7002tw@gmail.com>
2026-02-18 00:30:45 +0800

    改一下路徑

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,5 @@ export default defineConfig({
 
   base: process.env.NODE_ENV === 'production' ? '/reactHW03/' : '/',
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import'],
-      },
-    },
-  },
+
 })
